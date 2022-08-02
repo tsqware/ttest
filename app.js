7d027690bb090e8e1d1b3585373fa2e0cec42ccf
Terrence Tyson <tsqware@gmail.com>
2022-08-01 22:07:51 -0400

    set values for dev

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://tsqware-a07f0.web.app');
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // https://tsqware-a07f0.web.app
 	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
